3b0fa8e38e85140ef7f004f947a1fa23d437565b
espadv69 <aitorjob69@gmail.com>
2025-01-13 14:05:43 +0100

    Integrate Calculator component and global styles into main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { Calculator } from './components/Calculator.jsx'
+import './components/css/global.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-  <div>
-    <h1>Hello, world</h1>
-  </div>
+  <React.StrictMode>
+    <Calculator />
+  </React.StrictMode>
 )
