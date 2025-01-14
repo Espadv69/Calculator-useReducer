7e55925868507b41395e218047f89967a956dc35
espadv69 <aitorjob69@gmail.com>
2025-01-14 12:18:17 +0100

    Enhance ADD_DIGIT action in Calculator reducer to handle edge cases for zero and decimal input

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -11,11 +11,15 @@ const reducer = (state, action) => {
 
   switch (type) {
     case 'ADD_DIGIT':
+      // If the current value is 0 and the payload is 0, return the state as is
       if (state.currentValue === '0' && payload === 0) return state
+
+      // If the current value is 0 and the payload is not a dot, return the payload as the new current value
       if (state.currentValue === '0' && payload !== '.') {
         return { ...state, currentValue: payload }
       }
 
+      // If the current value already has a dot and the payload is a dot, return the state as is
       return { ...state, currentValue: state.currentValue + payload }
   }
 }
